3a8aee5b1942c10fada61c2e286cf2953935a78b
Abdul <diazabdulm@gmail.com>
2019-03-29 02:18:09 -0400

    made significant progress towards date and timezone parsing and date extraction from client to server

--- routes/index.js
+++ routes/index.js
@@ -2,38 +2,93 @@ var express = require('express');
 var router = express.Router();
 var admin = require("firebase-admin");
 var serviceAccount = require("../serviceAccountKey.json");
-
-// Init Firebase Admin
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://nomail-f7abe.firebaseio.com"
-});
+var nlp = require('compromise');
+var moment = require('moment');
+var moment_tz = require('moment-timezone');
 
 
-function parseDate(date) {
-  return moment.utc(date).format();
-}
+var smartProcessing = {
+  mount: function () {
+    this.initFirebase();
+  },
+  initFirebase: function () {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: "https://nomail-f7abe.firebaseio.com"
+    });
+  },
+  processTask: function (task, timezone, today) {
+    var date = this.processDate(task, timezone, today);
+    console.log(date.format());
+  },
+  processDate: function (task, timezone, today) {
+    var nlpDate = nlp(task).dates().data();
+    var dayINeed = nlpDate[0].date.weekday;
 
+    switch (true) {
+      case (dayINeed !== null):
+        return this.processWeekday(dayINeed, timezone, today);
+        break;
+      default:
+        console.error('An error ocurred');
+        break;
+    }
+    // console.log(nlpDate)
 
-function sendMessage(data) {
-  data = JSON.parse(data);
-
-  var UTCdate = parseDate(data.date);
-  var message = {
-    data: {
-
-    },
-    token: token
+  },
+  processWeekday: function (dayINeed, timezone, today) {
+    if (today <= dayINeed) {
+      // then just give me this week's instance of that day
+      return moment().isoWeekday(dayINeed);
+    } else {
+      // otherwise, give me *next week's* instance of that same day
+      return moment().add(1, 'weeks').isoWeekday(dayINeed);
+    }
   }
-
-  admin.messaging().send(message)
 }
+
+// init
+smartProcessing.mount()
+
+
+// function parseDate(date) {
+//   return moment.utc(date).format();
+// }
+
+
+// function getNextWeekday(today, dayIneed) {
+
+// }
+
+// function sendMessage(data) {
+//   data = JSON.parse(data);
+
+//   var UTCdate = parseDate(data.date);
+//   var message = {
+//     data: {
+
+//     },
+//     token: token
+//   }
+
+//   admin.messaging().send(message)
+// }
+
+
+// function processTask(task) {
+//   var date = processDate(task);
+// }
+
+// TODO: put a checker for named dates like 'today', 'tomorrow', 'this weekend', 'christmas', etc.
 
 
 /* POST sent task */
 router.post('/', function (req, res, next) {
-  console.log(req.body);
-  sendMessage(token);
+  var task = req.body.task;
+  var timezone = req.body.timezone;
+  var today = req.body.today;
+  smartProcessing.processTask(task, timezone, today);
 });
+
 
 module.exports = router;
